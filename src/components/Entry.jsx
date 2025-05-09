6e9e19ab26e87b169c0741b50d4cb6ea8fd0c9b0
nika robakidze <nika70252@gmail.com>
2025-05-09 15:09:11 +0400

    props

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,23 +1,23 @@
-export default function Entry(){
+export default function Entry(props){
     return(
         <section class = "card-section">
             <div class = "card-container">
                 <img 
                     className="main-image"
-                    src="https://scrimba.com/links/travel-journal-japan-image-url" 
-                    alt="mount fuji" 
+                    src={props.img.src}
+                    alt={props.img.alt}
                     />
                 <div class="main-content">
                     <div class="card-header">
                         <div class="card-name">
                             <img src="./public/image.png"/>
-                            <p>JAPAN</p>
+                            <p>{props.country}</p>
                         </div>
                         <a>View on Google Maps</a>
                     </div>
-                    <h1>Mount Fuji</h1>
-                    <p class="date">12 Jan, 2021 - 24 Jan, 2021</p>
-                    <p class="card-info">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                    <h1>{props.title}</h1>
+                    <p class="date">{props.dates}</p>
+                    <p class="card-info">{props.text}</p>
                 </div>
             </div>
         </section>
