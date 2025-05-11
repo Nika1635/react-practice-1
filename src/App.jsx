d35fdefb0b513e2128f40255f7a0d8ce312011f4
nika robakidze <nika70252@gmail.com>
2025-05-11 14:01:01 +0400

    last

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,8 @@ import data from "./data.js"
 export default function App(){
     const mountData = data.map(info => {
         return <Entry
-                img={{ 
-                    src: info.img.src,
-                    alt: info.img.src 
-                }}
-                title= {info.title}
-                country={info.country}
-                googleMapsLink={info.googleMapsLink}
-                dates={info.dates}
-                text={info.text}
+                key = {info.id}
+                {...info}
                 />
     })
 
